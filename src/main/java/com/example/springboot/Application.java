c06f4c95f904bcd54a32aee0404cfc3d9964a11f
Arun Krishnamurthy <arun.km.279@gmail.com>
2020-01-20 08:17:58 -0600

    fixed arrays error on application.java
    arrays import was not available

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -3,6 +3,8 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
